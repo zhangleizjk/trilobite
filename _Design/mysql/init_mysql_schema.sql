936a4b20a8d3640bcd412f55ce31c2be2ba020b9
profoundblue <profoundblue@163.com>
2017-03-20 19:07:26 +0800

    0002+

--- _Design/mysql/init_mysql_schema.sql
+++ _Design/mysql/init_mysql_schema.sql
@@ -25,17 +25,14 @@ create table if not exists `customer`
 /*
  * 
  */
-drop table if exists `delivery_address`;
-create table if not exists `delivery_address`
+drop table if exists `customer_browse_history`;
+create table if not exists `customer_browse_history`
 (
 	`id` bigint unsigned auto_increment not null,
 	`customer_id` bigint unsigned not null,
-	`consignee` varchar(50) character set utf8 collate utf8_bin not null,
-	`phone` varchar(20) character set utf8 collate utf8_bin  not null,
-	`basic_address` varchar(100) character set utf8 collate utf8_bin not null,
-	`detail_address` varchar(100) character set utf8 collate utf8_bin not null,
-	`is_default` boolean default true,
+	`product_id` bigint unsigned not null,
 	`add_time` timestamp default current_timestamp,
+	unique(`customer_id`, `product_id`),
 	primary key(`id`)
 )
 	engine InnoDB
@@ -45,26 +42,8 @@ create table if not exists `delivery_address`
 /*
  * 
  */
-drop table if exists `shopping_cart`;
-create table if not exists `shopping_cart`
-(
-	`id` bigint unsigned auto_increment not null,
-	`customer_id` bigint unsigned not null,
-	`product_id` bigint unsigned not null,
-	`quantity` bigint unsigned default 1,
-	`add_time` timestamp default current_timestamp,
-	unique(`customer_id`, `product_id`),
-	primary key(`id`)
-)
-	engine InnoDB
-	default character set utf8
-	default collate utf8_bin;
-
-/*
- * 
- */
-drop table if exists `customer_browsing_history`;
-create table if not exists `customer_browsing_history`
+drop table if exists `customer_favourite`;
+create table if not exists `customer_favourite`
 (
 	`id` bigint unsigned auto_increment not null,
 	`customer_id` bigint unsigned not null,
@@ -76,12 +55,12 @@ create table if not exists `customer_browsing_history`
 	engine InnoDB
 	default character set utf8
 	default collate utf8_bin;
-
+	
 /*
  * 
  */
-drop table if exists `wish_list`;
-create table if not exists `wish_list`
+drop table if exists `customer_wish`;
+create table if not exists `customer_wish`
 (
 	`id` bigint unsigned auto_increment not null,
 	`customer_id` bigint unsigned not null,
@@ -97,19 +76,58 @@ create table if not exists `wish_list`
 /*
  * 
  */
-drop table if exists `wish_list_detail`;
-create table if not exists `wish_list_detail`
+drop table if exists `customer_wish_detail`;
+create table if not exists `customer_wish_detail`
 (
 	`id` bigint unsigned auto_increment not null,
-	`wish_list_id` bigint unsigned not null,
+	`wish_id` bigint unsigned not null,
 	`product_id` bigint unsigned not null,
 	`add_time` timestamp default current_timestamp,
+	unique(`wisht_id`, `product_id`),
 	primary key(`id`)
 )
 	engine InnoDB
 	default character set utf8
 	default collate utf8_bin;	
 	
+/*
+ * 
+ */
+drop table if exists `customer_cart`;
+create table if not exists `customer_cart`
+(
+	`id` bigint unsigned auto_increment not null,
+	`customer_id` bigint unsigned not null,
+	`product_id` bigint unsigned not null,
+	`quantity` bigint unsigned default 1,
+	`add_time` timestamp default current_timestamp,
+	unique(`customer_id`, `product_id`),
+	primary key(`id`)
+)
+	engine InnoDB
+	default character set utf8
+	default collate utf8_bin;
+	
+/*
+ * 
+ */
+drop table if exists `customer_delivery_address`;
+create table if not exists `customer_delivery_address`
+(
+	`id` bigint unsigned auto_increment not null,
+	`customer_id` bigint unsigned not null,
+	`receiver` varchar(50) character set utf8 collate utf8_bin not null,
+	`phone` varchar(20) character set utf8 collate utf8_bin  not null,
+	`primary_address` varchar(100) character set utf8 collate utf8_bin not null,
+	`detail_address` varchar(100) character set utf8 collate utf8_bin not null,
+	`is_default` boolean default true,
+	`add_time` timestamp default current_timestamp,
+	primary key(`id`)
+)
+	engine InnoDB
+	default character set utf8
+	default collate utf8_bin;
+
 /*
  * 
  */
@@ -189,6 +207,87 @@ create table if not exists `gift_card_recharge`
 /*
  * 
  */
+drop table if exists `customer_message_type`;
+create table if not exists `customer_message_type`
+(
+	`id` bigint unsigned auto_increment not null,
+	`name` varchar(50) character set utf8 collate utf8_bin,
+	unique(`name`),
+	primary key(`id`)
+)
+	engine InnoDB
+	default character set utf8
+	default collate utf8_bin;			
+insert into `customer_message_type`(`name`) values('important_message'), ('order_message'), ('account_message'), ('other_message');
+
+/*
+ * 
+ */
+drop table if exists `customer_message`;
+create table if not exists `customer_message`
+(
+	`id` bigint unsigned auto_increment not null,
+	`type_id` bigint unsigned not null,
+	`customer_id`bigint unsigned not null,
+	`title` varchar(100) character set utf8 collate utf8_bin,
+	`content` varchar(500) character set utf8 collate utf8_bin,
+	`add_time` timestamp default current_timestamp,
+	`status` enum('unread', 'read')
+	unique(`card_id`),
+	unique(`card_number`),
+	primary key(`id`)
+)
+	engine InnoDB
+	default character set utf8
+	default collate utf8_bin;			
+	
+/*
+ * 
+ */
+drop table if exists `recycle_customer_message`;
+create table if not exists `recycle_customer_message`
+(
+	`id` bigint unsigned auto_increment not null,
+	`type_id` bigint unsigned not null,
+	`customer_id`bigint unsigned not null,
+	`title` varchar(100) character set utf8 collate utf8_bin,
+	`content` varchar(500) character set utf8 collate utf8_bin,
+	`send_time` timestamp not null,
+	`status` enum('unread', 'read'),
+	`delete_time` timestamp default current_timestamp,
+	`delete_operator` enum('customer', 'root'),
+	unique(`card_id`),
+	unique(`card_number`),
+	primary key(`id`)
+)
+	engine InnoDB
+	default character set utf8
+	default collate utf8_bin;			
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+/*
+ * 
+ */
 drop table if exists `growth_as_grade`;
 create table if not exists `growth_as_grade`
 (
@@ -212,12 +311,16 @@ drop table if exists `money_as_growth`;
 create table if not exists `money_as_growth`
 (
 	`id` bigint unsigned auto_increment not null,
-	
+	`money_num` bigint unsigned not null,
+	`growth_num` bigint unsigned not null,
+	unique(`money_num`),
+	unique(`growth_num`),
 	primary key(`id`)
 )
 	engine InnoDB
 	default character set utf8
 	default collate utf8_bin;			
+insert into `money_as_growth`(`money_num`, `growth_num`) values(100, 100);
 	
 	
 	
